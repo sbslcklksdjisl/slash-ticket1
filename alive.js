efd3eda5d24ab328079a91f5af45c15dcd2302e6
sbslcklksdjisl <154664032+sbslcklksdjisl@users.noreply.github.com>
2024-02-17 12:08:37 +0300

    First Commit
    ouhiu

---
+++ alive.js
@@ -0,0 +1,12 @@
+{
+  "ticketCategoryID": "ايدي_الكاتاجوري",
+  "ticketNumber": 2,
+  "BACKGROUND": "صورة_الباجراوند",
+  "SECTIONS": [
+    {
+      "name": "اسم_القسم",
+      "roleID": "ايدي_الرتبة",
+      "image": "صورة_القسم"
+    }
+  ]
+}
